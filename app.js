2f9edbd3fad6fda5e4cfade3136ff4bb74a1821b
JEAN LOUIS DOS SANTOS <jeanlouis.dossantos84@gmail.com>
2021-05-12 08:57:18 +0200

    test formation openclassroom

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
+const productrouter = require("./router/products")
+
+const result = require('dotenv').config()
+console.log(result.parsed)
 const app = express();
 
-mongoose.connect('mongodb+srv://Admin:Admin@cluster0.vig5u.mongodb.net/sopeckocko_sauce?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    process.env.DB_HOST,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+app.use(bodyparser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-
-app.use('/test/:id/:name' , (req,res)=>{
-  console.log(req.params)
-  res.json({message : "test 2"})
-  });
-
-app.use((req,res)=>{
-    res.json({message : "test ok"})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
+
+
+app.use('/api/products', productrouter)
+
+
+
+// app.use('/test/:id/:name' , (req,res)=>{
+//   console.log(req.params)
+//   res.json({message : "test 2"})
+//   });
+
+// app.use((req,res)=>{
+//     res.json({message : "test ok"})
+// });
 
 module.exports = app;
---
+++ router/products.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/product");
+
+
+router.get("/", (req, res, next) => {
+    Product.find()
+      .then((products) => res.status(200).json({ products }))
+      .catch((error) => res.status(400).json({ error }));
+  });
+  
+router.get("/:id", (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+      .then((product) => res.status(200).json({ product }))
+      .catch((error) => res.status(400).json({ error }));
+  });
+  
+router.put("/:id", (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id, })
+      .then(() => res.status(200).json({ message: "Modified!" }))
+      .catch((error) => res.status(400).json({ error }));
+  });
+  
+router.delete("/:id", (req, res, next) =>
+    Product.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: "supprimé" }))
+      .catch((error) => res.status(400).json({ error }))
+  );
+  
+router.post("/", (req, res, next) => {
+    const product = new Product({ ...req.body });
+    product
+      .save()
+      .then((product) => {
+        res.status(201).json({ product });
+      })
+      .catch((error) => res.status(400).json({ error }));
+  });
+
+
+
+
+module.exports = router
